6ea9c8b5214ee212bb8a7c406c87bbd13d08410c
fANZYo <www.iehl@gmail.com>
2020-10-29 17:37:47 +0000

    Add responsive and lazy images

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,37 @@
-const modules = [
-	{ class: '.js-test', path: '/js/test.js' },
-];
+const selectiveJS = () => {
+	const modules = [
+		{ class: '.js-test', path: '/js/test.js' },
+	];
 
-modules.forEach((module) => {
-	const element = document.querySelector(module.class);
+	modules.forEach((module) => {
+		const element = document.querySelector(module.class);
 
-	if (element) {
-		const scriptTag = document.createElement('script');
-		scriptTag.src = module.path;
+		if (element) {
+			const scriptTag = document.createElement('script');
+			scriptTag.src = module.path;
 
-		document.body.appendChild(scriptTag);
-	}
-});
+			document.body.appendChild(scriptTag);
+		}
+	});
+};
+
+const lazyIMG = (imgs) => {
+	const loadIMG = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				observer.unobserve(entry.target);
+				entry.target.setAttribute('srcset', entry.target.dataset.srcset);
+			}
+		});
+	};
+
+	const observer = new IntersectionObserver(loadIMG);
+	imgs.forEach((img) => observer.observe(img));
+};
+
+selectiveJS();
+
+window.onload = () => {
+	const lazyIMGs = document.querySelectorAll('.js-lazy-img');
+	lazyIMG(lazyIMGs);
+};
