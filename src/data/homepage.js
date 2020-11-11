391b13d541a0066e4635c5f876023066548a06ec
fANZYo <www.iehl@gmail.com>
2020-11-11 16:22:43 +0000

    Add homepage content

--- src/data/homepage.js
+++ src/data/homepage.js
@@ -4,11 +4,34 @@ module.exports = () => {
 	return {
 		header: {
 			title: 'Creating for the users',
-			subline: 'Accessibility and general user experience are the fundamental principles guiding all our work from start to finish',
-			cta: 'let\'s talk!',
+			subline: 'Making something your users will love to use, no matter who your users are.',
+			ctas: {
+				more: 'Read more',
+				talk: 'Let\'s talk!',
+			},
 		},
 		usp: {
-			title: 'The how',
+			title: 'Working with us',
+			sections: [
+				{
+					title: 'Genuine user empathy',
+					text: 'With the web being such an integral part of everyday life, there is nothing worse than a painful or even unusable online experience. Your users and their needs are at the center of our attention to guarantee they have the best user experience the web can offer.',
+					illustation: 'https://placekitten.com/160/90',
+				},
+				{
+					title: 'Uncompromised quality',
+					text: 'Whether it is user experience, reliability, or maintainability, it is all underpinned by quality. We pride ourselves in delivery quality work that exceeds expectations.',
+					illustation: 'https://placekitten.com/160/90',
+				},
+				{
+					title: 'Shared interests',
+					text: 'What matters to you matters to us, whether it is cattering for a new audience or improving on some metrics, we\'ve got you.',
+					illustation: 'https://placekitten.com/160/90',
+				},
+				{
+					more: 'Read more',
+				},
+			],
 		},
 		portfolio: {
 			title: 'The what',
--- src/data/projects.js
+++ src/data/projects.js
@@ -12,6 +12,17 @@ module.exports = () => {
 			url: '/portfolio/scope'
 		},
 		{
+			title: 'Wolves TV',
+			img: {
+				small: '/assets/spurs.webp?nf_resize=fit&w=350',
+				medium: '/assets/spurs.webp?nf_resize=fit&w=480',
+				large: '/assets/spurs.webp?nf_resize=fit&w=720',
+				alt: 'wolves tv',
+			},
+			content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			url: '/portfolio/wolves-tv'
+		},
+		{
 			title: 'Tottenham Hotspurs',
 			img: {
 				small: '/assets/spurs.webp?nf_resize=fit&w=350',
