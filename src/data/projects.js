febeba4b01bc968fb61d6bd6ef7b9d144488797f
fANZYo <www.iehl@gmail.com>
2021-06-25 00:34:54 +0100

    Fix project blurb typos

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ module.exports = () => {
 	return [
 		{
 			title: 'Scope',
-			blurb: 'Scope is one of the United Kingtoms\' leading disability charity in England and Wales. They have campaigned for over fifty years to challenge negative attitudes and opinions about disability, to support those that need help, and to bring \'everyday equality\' for disabled people. They are a strong community of disabled and non-disabled people with a shared vision of equality.',
+			blurb: 'Scope is one of United Kingtoms\' leading disability charity in England and Wales. They have campaigned for over fifty years to challenge negative attitudes and opinions about disability, to support those that need help, and to bring "everyday equality" for disabled people. They are a strong community of disabled and non-disabled people with a shared vision of equality.',
 			role: ['Development', 'Accessibility', 'Performance', 'Architecture'],
 			url: 'https://www.scope.org.uk/',
 			img: {
@@ -53,7 +53,7 @@ module.exports = () => {
 		},
 		{
 			title: 'Spurs',
-			blurb: 'As one of England’s biggest and proudest football clubs, Tottenham Hotspur F.C. are \'hungry and progressive\'. They aimed to create a world class digital experience to match they\'re new market leading stadium, and to bring the fans to the heart of the club.',
+			blurb: 'As one of England’s biggest and proudest football clubs, Tottenham Hotspur F.C. are "hungry and progressive". They aimed to create a world class digital experience to match their new market leading stadium, and to bring the fans to the heart of the club.',
 			role: ['Development', 'Performance'],
 			url: 'https://www.tottenhamhotspur.com/',
 			img: {
@@ -104,7 +104,7 @@ module.exports = () => {
 		},
 		{
 			title: 'Wolves TV',
-			blurb: 'Wolverhampton Wanderers F.C., another one of England\'s top football clubs. To bring the latest news and videos to their fans, Wolves needed a new modern platform that matches the needs of their users. Wolves TV is a dedicated video streaming platform that offers fans an alternative ways to follow their favorite team.',
+			blurb: 'Wolverhampton Wanderers F.C., another one of England\'s top football clubs. To bring the latest news and videos to their fans, Wolves needed a new modern platform that serves the needs of their users. Wolves TV is a dedicated video streaming platform that offers fans an alternative way to follow their favorite team.',
 			role: ['Development'],
 			url: 'https://tv.wolves.co.uk/',
 			img: {
