4c1dde0f2cdd30b3aa57bc323b36fce87a2709c4
fANZYo <www.iehl@gmail.com>
2021-06-24 23:05:51 +0100

    Add copyright to footer

--- src/data/footer.js
+++ src/data/footer.js
@@ -3,5 +3,6 @@ module.exports = () => {
 		title: 'Contacts',
 		label: 'Get <em>Accessibility</em> and <em>Performance</em> tips',
 		placeholder: 'Email',
+		copyright: 'William IEHL © 2021 - Today',
 	};
 };
