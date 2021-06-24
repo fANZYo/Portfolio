fa9ba9ba2d09b590cc40cf6fb093b72aad66b376
fANZYo <www.iehl@gmail.com>
2021-06-24 18:34:02 +0100

    Write copy for project blurbs

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ module.exports = () => {
 	return [
 		{
 			title: 'Scope',
-			blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+			blurb: 'Scope is one of the UK\'s leading disability charity in England and Wales. They have campaigned for over fifty years to challenge negative attitudes and opinions about disability, to support those that need help, and to bring \'everyday equality\' for disabled people. They are a strong community of disabled and non-disabled people with a shared vision of equality.',
 			role: ['Development', 'Accessibility', 'Performance', 'Architecture'],
 			url: 'https://www.scope.org.uk/',
 			img: {
@@ -53,7 +53,7 @@ module.exports = () => {
 		},
 		{
 			title: 'Spurs',
-			blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+			blurb: 'As one of England’s biggest and proudest football clubs, Tottenham Hotspurs F.C. are \'hungry and progressive\'. They aimed to create a world class digital experience to match they\'re new market leading stadium, and to bring the fans to the heart of the club.',
 			role: ['Development', 'Performance'],
 			url: 'https://www.tottenhamhotspur.com/',
 			img: {
@@ -104,7 +104,7 @@ module.exports = () => {
 		},
 		{
 			title: 'Wolves TV',
-			blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+			blurb: 'Wolverhampton Wanderers F.C., another one of England\'s top football clubs. To bring the latest news and videos to their fans, they needed a new modern platform that matches the needs of their users. Wolves TV is a dedicated video streaming platform that offers fans alternative ways to follow their favorite team.',
 			role: ['Development'],
 			url: 'https://tv.wolves.co.uk/',
 			img: {
