efcf32f64bab9938c23e1004563f1a6987a3da6d
William IEHL <www.iehl@gmail.com>
2023-01-25 00:28:58 +0100

    Remove theme toggle

--- src/js/index.js
+++ src/js/index.js
@@ -14,35 +14,6 @@ const lazyIMG = (imgs) => {
 	imgs.forEach((img) => observer.observe(img));
 };
 
-const initThemeToggle = () => {
-	const toggler = document.querySelector('.js-contrast');
-
-	const themes = {
-		light: {
-			label: 'Switch to dark mode',
-			next: 'dark',
-		},
-		dark: {
-			label: 'Switch to light mode',
-			next: 'light',
-		},
-	};
-	toggler.setAttribute('aria-label', themes[document.documentElement.dataset.theme].label);
-
-	const setTheme = () => {
-		const { theme } = document.documentElement.dataset;
-
-		window.__setTheme(themes[theme].next, () => {
-			window.localStorage.setItem('WI_theme', themes[theme].next);
-			toggler.setAttribute('aria-label', themes[theme].label);
-		});
-	}
-
-	toggler.addEventListener('click', () => {
-		setTheme();
-	});
-};
-
 const initHamburgerMenu = () => {
 	const menu = document.querySelector('.js-menu');
 	const nav = document.querySelector('.js-nav');
@@ -102,7 +73,6 @@ const initHamburgerMenu = () => {
 
 window.onload = () => {
 	initHamburgerMenu();
-	initThemeToggle();
 
 	const lazyIMGs = document.querySelectorAll('.js-lazy-img');
 	lazyIMG(lazyIMGs);
