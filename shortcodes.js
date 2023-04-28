d3d7e56f908212c03d5fa7811713dcef4607b954
William IEHL <www.iehl@gmail.com>
2023-04-28 16:04:36 +0200

    Empty commit

--- shortcodes.js
+++ shortcodes.js
@@ -2,15 +2,15 @@ module.exports = (config) => {
   config.addNunjucksShortcode('GTAG', () => {
     if (process.env.GA_ENABLED) {
       return `
-				<!-- Global site tag (gtag.js) - Google Analytics -->
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-K9NHEFPWPW"></script>
-				<script>
-				  window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K9NHEFPWPW"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-					gtag('config', 'G-K9NHEFPWPW');
-				</script>
+          gtag('config', 'G-K9NHEFPWPW');
+        </script>
       `;
     }
 
